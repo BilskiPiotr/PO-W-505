2f029496609f003c52f1731ccb93d8e56c148a03
Piotr Bilski <bilski.piotr@outlook.com>
2022-04-14 18:05:39 +0200

    Uaktualniono opisy

--- PO-W-505/Program.cs
+++ PO-W-505/Program.cs
@@ -16,20 +16,23 @@ namespace PO_W_505
     //{ }
 
     // ========  2  ========
-    class Ssak
+    // I tak dla klasy HomoSapiens będą to na przykład
+    // wartość jednego z podstawowych elementów różnicujących,
+    // czyli średni współczynnik encefalizacji oraz ilość nóg,
+    // co zadeklarować można w następujący sposób 
+    class HomoSapiens : Primatomorpha
     {
-        int _auditoryOssicles = 3;
-        int _cervicalVertebrae = 7;
-        public int AuditoryOssicles
-        {
-            get => _auditoryOssicles;
-        }
-        public int CervicalVertebrae
-        {
-            get => _cervicalVertebrae;
-        }
+        float _skullCapacity = 7.6F;
+        int _legsNumber = 2;
+        public float SkullCapacity { get => _skullCapacity; }
+        public int LegsNumber { get => _legsNumber; }
     }
 
+    // Na kolejnym poziomie dziedziczenia mamy Naczelne.
+    // Po naczelnych może na przykład dziedziczyć
+    // umiejscowienie oczu z przodu twarzoczaszki
+    // oraz pięciopalczaste kończyny,
+    // co w przełożeniu na język programowania można zapisać tak:
     class Primatomorpha : Ssak
     {
         string[] _eyesLocation;
@@ -47,15 +50,31 @@ namespace PO_W_505
         }
         public int NbOfFingers { get => _nbOfFingers; }
     }
- 
-    class HomoSapiens : Primatomorpha
-    { 
-        float _skullCapacity = 7.6F;
-        int _legsNumber = 2;
-        public float SkullCapacity { get => _skullCapacity;}
-        public int LegsNumber { get => _legsNumber; }
+
+    // Wreszcie Ssaki. Po nich też co nieco dziedziczy nasza Scarlett.
+    // Niech to będzie ilość kręgów szyjnych i kosteczek słuchowych,
+    // a zapisać to można tak:
+    class Ssak
+    {
+        int _auditoryOssicles = 3;
+        int _cervicalVertebrae = 7;
+        public int AuditoryOssicles
+        {
+            get => _auditoryOssicles;
+        }
+        public int CervicalVertebrae
+        {
+            get => _cervicalVertebrae;
+        }
     }
- 
+
+    // Pozostała nam jeszcze klasa główna, czyli klasa Woman,
+    // ponieważ nasza Scarlett będzie jedynym i niepowtarzalnym
+    // obiektem tego właśnie typu.
+    // Można umieścić tu wiele cech charakterystycznych,
+    // ale powiedzmy, że na potrzeby naszej konstrukcji wystarczy,
+    // że damy możliwość ustalenia imienia, koloru włosów i wzrostu.
+    // W takim wypadku klasa Woman mogła by przyjąć taką postać.
     class Woman : HomoSapiens
     {
         string _name;
@@ -84,6 +103,11 @@ namespace PO_W_505
         }
     }
 
+    // W celu sprawdzenia jak zadziała nasze dziedziczenie
+    // wymodelujemy treść metody Main tak,
+    // aby powołać do życia w niej obiekt klasy Woman
+    // i pokazać wartości wszystkich pól z klasy własnej
+    // oraz nadrzędnych jakie dziedziczy. 
     class Program
     {
         static void Main(string[] args)
